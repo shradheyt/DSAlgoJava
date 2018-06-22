eaa85946d14207681e168075eebe80055b95ba1e
Shradhey Tripathi <shradhey029tripathi@gmail.com>
2018-06-23 04:03:47 +0530

    Linked List Implementation Added in Java

---
+++ src/com/shradhey/LinkedList/Node.java
@@ -0,0 +1,34 @@
+package com.shradhey.LinkedList;
+
+/**
+ * Created by Shradhey Tripathi on 23-Jun-18.
+ */
+public class Node<T extends Comparable<T>> {
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public String toString() {
+        return this.data.toString();
+    }
+
+}
