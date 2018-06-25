83776172347068a402ba2816b589ee3215e8592e
Shradhey Tripathi <shradhey029tripathi@gmail.com>
2018-06-26 02:00:47 +0530

    Queue Implementation using Linked List

---
+++ src/com/shradhey/Queue/Node.java
@@ -0,0 +1,33 @@
+package com.shradhey.Queue;
+
+/**
+ * Created by Shradhey Tripathi on 26-Jun-18.
+ */
+public class Node<T extends Comparable<T>> {
+
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+    public String toString() {
+        return this.data.toString();
+    }
+}
